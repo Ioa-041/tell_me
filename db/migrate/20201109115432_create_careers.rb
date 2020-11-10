371b7032c7706d1b97e606dc9a86ea5651ecb5e3
Ioa-041 <scraggly15@gmail.com>
2020-11-10 19:25:45 +0900

    careersテーブル生成

---
+++ db/migrate/20201109115432_create_careers.rb
@@ -0,0 +1,8 @@
+class CreateCareers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :careers do |t|
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
