1961a9bd57b9592553d2ec99878bc7c139bfe0a5
Ioa-041 <scraggly15@gmail.com>
2020-11-13 01:20:13 +0900

    CompanyWorkモデル生成、バリデーション設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,3 @@
 class Company < ApplicationRecord
   belongs_to :user
-  with_options presence: true do
-    validates :joining
-    validates :leaving
-    validates :name
-  end
 end
---
+++ app/models/company_work.rb
@@ -0,0 +1,17 @@
+class CompanyWork
+  include ActiveModel::Model
+  attr_accessor :joining_year, :joining_month, :leaving_year, :leaving_month, :name, :detail, :worker_number, :employment_type, :user_id, :everyday, :monthly, :sometimes
+
+  with_options presence: true do
+    validates :joining_year
+    validates :joining_month
+    validates :leaving_year
+    validates :leaving_month
+    validates :name
+  end
+
+  def save
+    company = Company.create(joining_year: joining_year, joining_month: joining_month, leaving_year: leaving_year, leaving_month: leaving_month, name: name, detail: detail, worker_number: worker_number, employment_type: employment_type, user_id: user_id)
+    Work.create(everyday: everyday, monthly: monthly, sometimes: sometimes, company_id: company.id)
+  end
+end
